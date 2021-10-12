865920e5d23f95394bf45f728748af1841b26d7d
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-12 07:17:01 -0300

    Criando Projeto
    Projeto de API criado no repositório github.

---
+++ API CidadesClientes/API CidadesClientes/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_CidadesClientes.Models
+{
+	public class Cliente
+	{
+		public Guid Id { get; set; }
+		public string Nome { get; set; }
+	}
+}
