47e7e84c663e80e03a7ebf9b8dd38e1b7278d625
Jefferson Frigo <jrfrigo1@gmail.com>
2021-10-15 10:26:57 -0300

    Atualizando informação de acesso ao Servidor
    O local do servidor estava para meu computador, foi mudado para localhost para agregar mais flexibilidade.

--- API CidadesClientes/API CidadesClientes/Startup.cs
+++ API CidadesClientes/API CidadesClientes/Startup.cs
@@ -28,7 +28,7 @@ namespace API_CidadesClientes
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ClienteDbContext>(Ops => Ops.UseSqlServer("Data Source=PCGRANDE\\SQLEXPRESS;" +
+			services.AddDbContext<ClienteDbContext>(Ops => Ops.UseSqlServer("Data Source=localhost\\SQLEXPRESS;" +
 				"database=ClientesDb;" +
 				"Integrated Security=True;" +
 				"Connect Timeout=5;" +
